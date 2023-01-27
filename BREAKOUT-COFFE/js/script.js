474f4ec21243260efc9d377602da2cdde3b7d677
hipsterx <93419520+hipsterx@users.noreply.github.com>
2023-01-28 02:19:45 +0700

    Add files via upload

---
+++ BREAKOUT-COFFE/js/script.js
@@ -0,0 +1,15 @@
+// Toggle Class active
+const navbarNav = document.querySelector(".navbar-nav");
+// klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//KLIK LUAR SIDEBAR
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
